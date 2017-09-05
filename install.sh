15c97ec5ad9326ae1974571917176e7cb25afba1
mohrf <fernandomohr@gmail.com>
2017-09-05 18:07:29 +0000

    Remove Ubuntu

--- install.sh
+++ install.sh
@@ -5,19 +5,14 @@ unamestr=$(uname -a)
 if [[ "$unamestr" == *'Darwin'* ]] 
 then 
   bash ./osx/install.sh;
-elif [[ "$unamestr" == *'Ubuntu'* ]]
+elif [[ "$unamestr" == *'Linux'* ]]
 then 
-  bash ./ubuntu/install.sh;
 fi
 
 bash ./commons/install.sh;
 
-
 echo "Copying tmux.conf"
 cp .tmux.conf ~/.tmux.conf
-
-echo "Copying ideavimrc"
-cp .ideavimrc ~/.ideavimrc
 
 echo "Done"
 
