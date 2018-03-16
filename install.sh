4f4dd5621d515b230dcda18fbeeb2b76a7e63810
mohrf <fernandomohr@gmail.com>
2018-03-16 00:21:35 -0300

    Remove unused software

--- install.sh
+++ install.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# Install software 
+# install software 
 unamestr=$(uname -a)
 if [[ "$unamestr" == *'Darwin'* ]] 
 then 
-  bash ./osx/install.sh;
+  bash ./macOS/install.sh;
 else 
   echo $unamestr" not supported" 
 fi
 
-bash ./commons/install.sh;
-
-echo "Copying tmux.conf"
-cp .tmux.conf ~/.tmux.conf
-
-echo "Done"
+echo "done"
 
 
---
+++ macOS/install.sh
@@ -0,0 +1,20 @@
+# install Homebrew
+which -s brew
+if [[ $? != 0 ]] ; then 
+  echo "installing homebrew..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "updating brew packages..."  
+  brew update
+fi
+
+# latex tools
+
+if ! brew cask ls --versions mactex > /dev/null ; then
+  echo "installing mactex..."  
+  brew cask install mactex
+fi
+echo "installing latex extension for vs-code..."
+code --install-extension James-Yu.latex-workshop
+
+
