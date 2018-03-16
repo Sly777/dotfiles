d02769603d1e4451a73ddfa4ad1046e42e857669
mohrf <fernandomohr@gmail.com>
2018-03-16 00:53:39 -0300

    Add node install

--- macOS/install.sh
+++ macOS/install.sh
@@ -9,12 +9,18 @@ else
 fi
 
 # latex tools
-
 if ! brew cask ls --versions mactex > /dev/null ; then
   echo "installing mactex..."  
   brew cask install mactex
 fi
 echo "installing latex extension for vs-code..."
 code --install-extension James-Yu.latex-workshop
+
+# node 
+if ! brew ls --versions node > /dev/null ; then
+  brew install node
+else 
+  brew upgrade node
+fi 
 
 
